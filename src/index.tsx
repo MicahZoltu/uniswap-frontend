4f33083dea59f1f487582575fc8c7dd046441cdf
Micah Zoltu <micah@zoltu.net>
2022-08-18 20:13:52 +0800

    Trying to bisect CI failure.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import '@reach/dialog/styles.css'
 import 'inter-ui'
 import 'polyfills'
-import 'components/analytics'
 
 import { FeatureFlagsProvider } from 'featureFlags'
 import { BlockNumberProvider } from 'lib/hooks/useBlockNumber'
