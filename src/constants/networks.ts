4a16d92f8b48f6f2485dd104d736c324de3e4da8
Micah Zoltu <micah@zoltu.net>
2022-08-19 15:07:01 +0800

    Trying to bisect CI failure.

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -3,7 +3,6 @@ import { JsonRpcProvider } from '@ethersproject/providers'
 import { SupportedChainId } from './chains'
 
 const INFURA_KEY = process.env.REACT_APP_INFURA_KEY
-
 export const MAINNET_PROVIDER = new JsonRpcProvider(`https://ethereum.keydonix.com/v1/mainnet`)
 
 /**
@@ -11,9 +10,9 @@ export const MAINNET_PROVIDER = new JsonRpcProvider(`https://ethereum.keydonix.c
  */
 export const RPC_URLS: { [key in SupportedChainId]: string } = {
   [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+  [SupportedChainId.RINKEBY]: `https://ethereum.keydonix.com/v1/rinkeby`,
+  [SupportedChainId.GOERLI]: `https://ethereum.keydonix.com/v1/goerli`,
   [SupportedChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.KOVAN]: `https://kovan.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.OPTIMISM]: `https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.OPTIMISTIC_KOVAN]: `https://optimism-kovan.infura.io/v3/${INFURA_KEY}`,
